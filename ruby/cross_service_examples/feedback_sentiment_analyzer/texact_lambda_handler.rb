0718558ce52f912ddaa22e062d2d1b14c3103e12
ford-at-aws <fprior@amazon.com>
2023-07-11 16:14:53 -0600

    new file

--- ruby/cross_service_examples/feedback_sentiment_analyzer/texact_lambda_handler.rb
+++ ruby/cross_service_examples/feedback_sentiment_analyzer/texact_lambda_handler.rb
@@ -1,40 +1,51 @@
-require 'json'
-require 'aws-sdk-textract'
-require 'logger'
-# require 'pry'
-
-def lambda_handler(event:, context:)
-
-  logger = Logger.new($stdout)
-
-  logger.info("event:\n #{event.to_s}\n")
-  logger.info("context:\n #{context.to_s}\n")
-
-  # Create an instance of the Textract client
-  client = Aws::Textract::Client.new(region: "us-east-1")
-
-  response = client.detect_document_text({
-                                           document: {
-                                             s3_object: {
-                                               bucket: event['detail']['bucket']['name'],
-                                               name: event['detail']['object']['key']
-                                             }
-                                           },
-                                         })
-  logger.info("#{response.to_s}\n")
-
-  extracted_words = []
+# This is a Lambfa function using the Textract client making x y calls
+# to the Textract API.
 
 
-  response.blocks.each do |obj|
-    if obj.block_type.include?('LINE')
-      if obj.respond_to?(:text) && obj.text
-        extracted_words.append(obj.text)
-      end
-    end
-  end
 
-  logger.info("extracted words: #{extracted_words.to_s}")
 
-  extracted_words.join(" ")
-end
+
+
+
+
+#
+# require 'json'
+# require 'aws-sdk-textract'
+# require 'logger'
+# # require 'pry'
+#
+# def lambda_handler(event:, context:)
+#
+#   logger = Logger.new($stdout)
+#
+#   logger.info("event:\n #{event.to_s}\n")
+#   logger.info("context:\n #{context.to_s}\n")
+#
+#   # Create an instance of the Textract client
+#   client = Aws::Textract::Client.new(region: "us-east-1")
+#
+#   response = client.detect_document_text({
+#                                            document: {
+#                                              s3_object: {
+#                                                bucket: event['detail']['bucket']['name'],
+#                                                name: event['detail']['object']['key']
+#                                              }
+#                                            },
+#                                          })
+#   logger.info("#{response.to_s}\n")
+#
+#   extracted_words = []
+#
+#
+#   response.blocks.each do |obj|
+#     if obj.block_type.include?('LINE')
+#       if obj.respond_to?(:text) && obj.text
+#         extracted_words.append(obj.text)
+#       end
+#     end
+#   end
+#
+#   logger.info("extracted words: #{extracted_words.to_s}")
+#
+#   extracted_words.join(" ")
+# end
